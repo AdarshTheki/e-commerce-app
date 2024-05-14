685c655c75db1c3cee2fbd7bb469c493ae6cc75e
Adarsh <adarshverma549@gmail.com>
2024-05-14 15:18:46 +0530

    "Added CORS headers to allow requests from Vercel deployment; updated proxy settings"

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ app.use(
             "http://localhost:5173",
             "http://localhost:8000",
             "http://localhost:3000",
-            "*"
+            "*",
         ],
         credentials: true,
     })
@@ -24,6 +24,19 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
 app.use(cookieParser());
+
+app.use((req, res, next) => {
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        "https://full-stack-ecommerce-app-page.vercel.app"
+    );
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization"
+    );
+    next();
+});
 
 // http://localhost:8000/api/v1/users
 
