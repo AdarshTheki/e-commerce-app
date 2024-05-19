b28c62dc7a5b8434ecb7e81ff01d29c7d7d36af2
Adarsh <adarshverma549@gmail.com>
2024-05-19 14:03:44 +0530

    Update backend dev script, frontend API base URL

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    server: {
-        proxy: {
-            '/url1': 'https://full-stack-ecommerce-app-sq9o.onrender.com',
-            '/url2': 'https://full-stack-ecommerce-api-pi.vercel.app',
-        },
-    },
     plugins: [react()],
 });
